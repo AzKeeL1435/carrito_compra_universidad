cd58b8cc4430fa1ecb099aaf1317d94792ab76e1
AzKeeL <167581949+AzKeeL1435@users.noreply.github.com>
2024-04-29 11:58:24 -1200

    Pedidos.py

--- Pedidos.py
+++ Pedidos.py
@@ -4,14 +4,14 @@ import random
 clear = lambda: os.system("cls")
 
 #Stock
-Pears = random.randint(0, 50)
-Apples = random.randint(0, 50)
-Oranges = random.randint(0, 50)
+pears = random.randint(0, 50)
+apples = random.randint(0, 50)
+oranges = random.randint(0, 50)
 
 #Prices
-Pear_price = 0.75
-Apple_price = 0.50
-Orange_price = 0.25
+pear_price = 0.75
+apple_price = 0.50
+orange_price = 0.25
 
 #User
 shopping_cart = [0, 0, 0]
@@ -27,32 +27,32 @@ while True:
         break
     elif menu_option == "1": # Agregar producto
         clear()
-        print(f"Fruta:  Cantidad\n1. Peras {Pears}\n2. Manzanas {Apples}\n3. Naranjas {Oranges}")
+        print(f"Fruta:  Cantidad\n1. peras {pears}\n2. manzanas {apples}\n3. Naranjas {oranges}")
         product_option = int(input("Ingrese el producto a agregar: "))
         if product_option == 1: # pears
             user_quantity = int(input("Ingrese la cantidad de peras: "))
-            if user_quantity <= Pears:
+            if user_quantity <= pears:
                 shopping_cart[0] += user_quantity
-                Pears -= user_quantity
+                pears -= user_quantity
             else:
                 print("no hay suficientes stock")
         if product_option == 2: # apples
             user_quantity = int(input("Ingrese la cantidad de Manzanas: "))
-            if user_quantity <= Apples:
+            if user_quantity <= apples:
                 shopping_cart[1] += user_quantity
-                Apples -= user_quantity
+                apples -= user_quantity
             else:
                 print("no hay suficientes stock")
         if product_option == 3: # oranges
             user_quantity = int(input("Ingrese la cantidad de Naranjas: "))
-            if user_quantity <= Oranges:
+            if user_quantity <= oranges:
                 shopping_cart[2] += user_quantity
-                Oranges -= user_quantity
+                oranges -= user_quantity
             else:
                 print("no hay suficientes stock")
     elif menu_option == "2": # Mostrar carrito
         clear()
-        print(f"Peras: {shopping_cart[0]}\nManzanas: {shopping_cart[1]}\nNaranjas: {shopping_cart[2]}\n\nSiendo un total de ${shopping_cart[0]*Pear_price + shopping_cart[1]*Apple_price + shopping_cart[2]*Orange_price}")
+        print(f"Peras: {shopping_cart[0]}\nManzanas: {shopping_cart[1]}\nNaranjas: {shopping_cart[2]}\n\nSiendo un total de ${shopping_cart[0]*pear_price + shopping_cart[1]*apple_price + shopping_cart[2]*orange_price}")
         input("Presione enter para salir")
         clear()
     elif menu_option == "3": # Pagar
@@ -60,7 +60,7 @@ while True:
         while True:
             identification = input("Ingrese su documento: ")
             if len(identification) >= 6 and len(identification) <= 12:
-                print(f"Numero de factura {random.randint(000000,999999)} a la identificacion {identification}\nPeras: {shopping_cart[0]}\nManzanas: {shopping_cart[1]}\nNaranjas: {shopping_cart[2]}\n\nSiendo un total de ${shopping_cart[0]*Pear_price + shopping_cart[1]*Apple_price + shopping_cart[2]*Orange_price}")
+                print(f"Numero de factura {random.randint(000000,999999)} a la identificacion {identification}\nPeras: {shopping_cart[0]}\nManzanas: {shopping_cart[1]}\nNaranjas: {shopping_cart[2]}\n\nSiendo un total de ${shopping_cart[0]*pear_price + shopping_cart[1]*apple_price + shopping_cart[2]*orange_price}")
                 input("presione enter para continuar")
                 clear()
                 shopping_cart = [0,0,0]
